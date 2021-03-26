14291ccda504b3249f1d26efb0565af1c2a9770a
ongods <75097279+ongods@users.noreply.github.com>
2021-03-25 20:06:41 -0400

    BALLS IN 6.2.3. GAS ASSEMBLY

---
+++ balls.s
@@ -0,0 +1,28 @@
+.text                           # section declaration
+
+			                         
+    .global _start              
+    
+
+_start:
+                              
+
+                                
+	  movl    $len,%edx           
+	  movl    $msg,%ecx           
+	  movl    $1,%ebx             
+	  movl    $4,%eax             
+	  int     $0x80               
+
+                                
+
+	  movl    $0,%ebx             
+	  movl    $1,%eax             
+	  int     $0x80               
+
+.data                           
+
+msg:
+	.ascii    "balls\n"   
+	len = . - msg                 
+#balls
